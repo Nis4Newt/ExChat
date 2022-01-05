2cc5b7cd9110f1ae94691a191acc427b91cad158
woong <woong@xlgames.com>
2022-01-05 19:51:30 +0900

    up

---
+++ lib/chat.ex
@@ -0,0 +1,21 @@
+defmodule Chat do
+
+  def receive_msg(msg) do
+    IO.puts msg
+  end
+
+  def send_msg(recipient, msg) do
+    spawn_task(__MODULE__, :receive_msg, recipient, [msg])
+  end
+
+  def spawn_task(module, fun, recipient, args) do
+    recipient
+    |> remote_supervisor
+    |> Task.Supervisor.async(module, fun, args)
+    |> Task.await()
+  end
+
+  def remote_supervisor(recipient) do
+    {Chat.TaskSupervisor, recipient}
+  end
+end
